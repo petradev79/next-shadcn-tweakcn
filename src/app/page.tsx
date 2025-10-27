b0d8153f7e16033f0ef5918aa2701dd76318a764
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2025-10-27 12:11:43 +0100

    add fetaures one section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/hero-section";
+import FeaturesOne from "@/components/features-one";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <FeaturesOne />
     </>
   );
 }
