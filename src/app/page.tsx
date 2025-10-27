c1ab56edf15e65db8d92b2930f1675e266a61283
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2025-10-27 12:30:27 +0100

    add customers section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/hero-section";
 import FeaturesOne from "@/components/features-one";
+import LogoCloud from "@/components/customers";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FeaturesOne />
+      <LogoCloud />
     </>
   );
 }
